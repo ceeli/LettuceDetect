5bf05c52d9c0f1e91c10fbc719cce546b9846b20
Markus Hovorka <markus@hovorka.cc>
2025-03-08 12:30:17 +0100

    Improve api serve script (add dev & prod modes)

--- api/serve.py
+++ api/serve.py
@@ -9,6 +9,16 @@ import subprocess
 def _argparse() -> dict:
     parser = argparse.ArgumentParser(description="Start lettucedetect Web API.")
     parser.add_argument(
+        "mode",
+        help=(
+            'Choose "dev" for development or "prod" for production environments. The serve script '
+            'uses "fastapi dev" for "dev" or "fastapi run" for "prod" to start the web server. '
+            'Additionally when choosing the "dev" mode, python modules can be directly imported '
+            "from the repositroy without installing the package."
+        ),
+        choices=["prod", "dev"],
+    )
+    parser.add_argument(
         "--model",
         help='Path or huggingface URL to the model. The default value is "KRLabsOrg/lettucedect-base-modernbert-en-v1".',
         default="KRLabsOrg/lettucedect-base-modernbert-en-v1",
@@ -24,15 +34,19 @@ def _argparse() -> dict:
 
 def _run_fastapi(args: dict) -> None:
     api_folder = pathlib.Path(__file__).parent.resolve()
+    repo_folder = api_folder.parent
     env = os.environ.copy()
     env["LETTUCEDETECT_MODEL"] = args.model
     env["LETTUCEDETECT_METHOD"] = args.method
+    if args.mode == "dev":
+        # Needed for fastapi to be able to import directly from the repository.
+        env["PYTHONPATH"] = env.get("PYTHONPATH", "") + os.pathsep + str(repo_folder)
+        fastapi_cmd = ["fastapi", "dev", api_folder / "main.py"]
+    else:
+        fastapi_cmd = ["fastapi", "run", api_folder / "main.py"]
     try:
-        # Ignore S603: Validate input to run method. False positive because
-        # api_folder is not input by the user.
-        # Ignore S607: Use relative path for fastapi. Unavoidable here because
-        # fastapi executable location is not known.
-        subprocess.run(["fastapi", "dev", api_folder / "main.py"], env=env)  # noqa: S603, S607
+        # Ignore S603: Validate input to run method. False positive.
+        subprocess.run(fastapi_cmd, env=env, cwd=repo_folder)  # noqa: S603
     except KeyboardInterrupt:
         pass
 
